161663e55cf83ac6e2a2e1cf7f5dcaa33e68e36e
강병안 <aosldk95@naver.com>
2019-07-08 16:07:42 +0900

    implement

--- nmap/src/main/java/com/kba/nmap/MainActivity.java
+++ nmap/src/main/java/com/kba/nmap/MainActivity.java
@@ -2,6 +2,7 @@ package com.kba.nmap;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
@@ -16,10 +17,24 @@ import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.ArrowheadPathOverlay;
 import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
+import com.o3dr.android.client.ControlTower;
+import com.o3dr.android.client.Drone;
+import com.o3dr.android.client.interfaces.DroneListener;
+import com.o3dr.android.client.interfaces.LinkListener;
+import com.o3dr.android.client.interfaces.TowerListener;
+import com.o3dr.services.android.lib.drone.property.Type;
+import com.o3dr.services.android.lib.gcs.link.LinkConnectionStatus;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, DroneListener, TowerListener, LinkListener {
+    private Drone drone;
+    private int droneType = Type.TYPE_UNKNOWN;
+    private ControlTower controlTower;
+    private final Handler handler = new Handler();
+
+    private static final int DEFAULT_UDP_PORT = 14550;
+    private static final int DEFAULT_USB_BAUD_RATE = 57600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,73 +53,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @UiThread
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        LatLngBounds bounds = new LatLngBounds.Builder()
-                .include(new LatLng(35.94528, 126.682167))
-                .include(new LatLng(35.969389, 126.957333))
-                .include(new LatLng(35.983, 126.717))
-                .include(new LatLng(35.846833, 127.129361))
-                .build();
-        CameraUpdate cameraUpdate = CameraUpdate.fitBounds(bounds,200);
-        naverMap.moveCamera(cameraUpdate);
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(35.94528, 126.682167));
-        marker.setMap(naverMap);
-        Marker marker1 = new Marker();
-        marker1.setPosition(new LatLng(35.969389, 126.957333));
-        marker1.setMap(naverMap);
-        Marker marker2 = new Marker();
-        marker2.setPosition(new LatLng(35.983, 126.717));
-        marker2.setMap(naverMap);
-        Marker marker3 = new Marker();
-        marker3.setPosition(new LatLng(35.846833, 127.129361));
-        marker3.setMap(naverMap);
-        ArrowheadPathOverlay arrowheadPath = new ArrowheadPathOverlay();
-        arrowheadPath.setCoords(Arrays.asList(
-                new LatLng(35.94528, 126.682167),
-                new LatLng(35.983, 126.717),
-                new LatLng(35.969389, 126.957333),
-                new LatLng(35.846833, 127.129361)
-        ));
-        Context context = this;
-        arrowheadPath.setMap(naverMap);
-        InfoWindow infoWindow = new InfoWindow();
-        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "군산대";
-            }
-        });
-        InfoWindow infoWindow1 = new InfoWindow();
-        infoWindow1.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "원광대";
-            }
-        });
-        InfoWindow infoWindow2 = new InfoWindow();
-        infoWindow2.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "군산시청";
-            }
-        });
-        InfoWindow infoWindow3 = new InfoWindow();
-        infoWindow3.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "전북대";
-            }
-        });
-        infoWindow.open(marker);
-        infoWindow1.open(marker1);
-        infoWindow2.open(marker2);
-        infoWindow3.open(marker3);
-
 
     }
 
+    @Override
+    public void onDroneEvent(String event, Bundle extras) {
+
+    }
+
+    @Override
+    public void onDroneServiceInterrupted(String errorMsg) {
+
+    }
+
+    @Override
+    public void onTowerConnected() {
+
+    }
+
+    @Override
+    public void onTowerDisconnected() {
+
+    }
+
+    @Override
+    public void onLinkStateUpdated(@NonNull LinkConnectionStatus connectionStatus) {
+
+    }
 }
