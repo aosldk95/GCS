e85e5206bfeb734f0afdc503768545b2a73c4d39
강병안 <aosldk95@naver.com>
2019-08-07 15:30:15 +0900

    타이틀바 제거

--- nmap/src/main/java/com/kba/nmap/MainActivity.java
+++ nmap/src/main/java/com/kba/nmap/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -85,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     int altit = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         Log.i(TAG, "Start mainActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
